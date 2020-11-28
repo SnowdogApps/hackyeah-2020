b5487db4a9fe43b4983b7376c86aaed99d10b705
rjwaberski <radek.waberski@snow.dog>
2020-11-28 07:37:49 +0100

    Webflow export test

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,298 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        hackyeah-2020
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="landing-page">
+    <div class="hero">
+      <div
+        data-collapse="medium"
+        data-animation="default"
+        data-duration="400"
+        data-doc-height="1"
+        role="banner"
+        class="nav w-nav"
+      >
+        <div class="nav-inner">
+          <div class="nav-logo-wrap">
+            <a href="#" class="brand w-nav-brand"><img src="images/logo-white.png" width="120" alt=""></a>
+          </div>
+          <div class="nav-menu-wrap">
+            <nav role="navigation" class="nav-menu-2 w-nav-menu">
+              <a href="#about" class="nav-link w-nav-link">About</a>
+              <a href="#features" class="nav-link w-nav-link">Features</a>
+              <a href="#how-to-use" class="nav-link w-nav-link">How to Use</a>
+              <a href="#download" class="nav-link w-nav-link">Download</a>
+            </nav>
+            <div class="menu-button w-nav-button">
+              <div class="menu-icon w-icon-nav-menu" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section center">
+      <img src="images/arrow.svg" alt="" class="arrow">
+      <div data-w-id="6b220cb1-4b9c-7fb0-75b9-53ff4a6c35a2" class="hero-content">
+        <h1 class="heading-3">
+          Hydro <br>Homies
+        </h1>
+        <h3 class="hero-subhead">
+          Manage your water smarter.
+        </h3>
+        <a href="#" class="button w-button">Get the App</a>
+      </div><img src="images/app-05.png" data-w-id="77e69727-c455-8e07-0990-833a661c94ea" alt="" class="hero-image">
+    </div>
+    <div id="about" class="section wide img">
+      <div data-w-id="b126983d-9126-fc06-c81d-1318685e2add" class="wrap">
+        <div class="title">
+          Aenean Consectetur Porta
+        </div>
+        <h2>Nullam quis risus eget urna mollis ornare vel eu leo.</h2>
+      </div>
+      <div class="wrapper">
+        <img src="images/dash-device.png" sizes="90vw" srcset="images/dash-device-p-500.png 500w, images/dash-device-p-800.png 800w, images/dash-device-p-1080.png 1080w, images/dash-device-p-1600.png 1600w, images/dash-device.png 1903w" alt="">
+      </div>
+    </div>
+    <div class="section">
+      <div id="features" class="feature-wrap">
+        <div data-w-id="4593c4b7-56e8-060a-be57-648b0e08fc65" class="feature-content">
+          <div class="title">
+            Aenean Consectetur
+          </div>
+          <h2 class="black">
+            Cay sociis nato que penatibus et
+          </h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+        </div>
+        <div class="feature-image">
+          <img src="images/app-06.png" width="341" alt="" class="image-2">
+        </div>
+      </div>
+      <div class="feature-wrap reverse">
+        <div data-w-id="cafe0045-d304-79d9-8f68-af3adaed06e9" class="feature-content">
+          <div class="title">
+            ConsectUltricies
+          </div>
+          <h2 class="black-text">
+            Amet Ipsum
+          </h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+        </div>
+        <div class="feature-image">
+          <img
+            src="images/app-05_1.png"
+            width="386"
+            sizes="(max-width: 479px) 90vw, (max-width: 991px) 40vw, 25vw"
+            srcset="images/app-05_1-p-500.png 500w, images/app-05_1.png 540w"
+            alt=""
+            class="image-2"
+          >
+        </div>
+      </div>
+      <div class="feature-wrap">
+        <div data-w-id="88086931-0bbf-a2f5-1fc3-58d79ba19b7e" class="feature-content">
+          <div class="title">
+            Aenean Consectetur Porta
+          </div>
+          <h2 class="black-text">
+            Magna Porta Sit Bibendum
+          </h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+        </div>
+        <div class="feature-image">
+          <img src="images/app-04.png" width="341" alt="" class="image-2">
+        </div>
+      </div>
+      <div class="feature-wrap icons">
+        <div class="feature-content icons">
+          <img src="images/icon-burger-magenta.svg" width="80" alt="" class="feature-icon">
+          <h4 class="black-text">
+            Activity Notifications
+          </h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+        </div>
+        <div class="feature-content icons">
+          <img src="images/icon-silverware-magenta.svg" width="74" alt="" class="feature-icon">
+          <h4 class="black-text">
+            Custom Timing
+          </h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+        </div>
+        <div class="feature-content icons">
+          <img src="images/icon-data-magenta.svg" alt="" class="feature-icon">
+          <h4 class="black-text">
+            Mobile Optimized
+          </h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. </p>
+        </div>
+      </div>
+      <div class="feature-wrap icons numbers">
+        <div class="feature-content icons numbers">
+          <div class="number">
+            +12k
+          </div>
+          <h4 class="black-text">
+            Nibh Elit Tristique
+          </h4>
+        </div>
+        <div class="feature-content icons numbers">
+          <div class="number">
+            84
+          </div>
+          <h4 class="black-text">
+            Aenean Condimentum
+          </h4>
+        </div>
+        <div class="feature-content icons numbers">
+          <div class="number">
+            3,07
+          </div>
+          <h4 class="black-text">
+            Quam Inceptos
+          </h4>
+        </div>
+        <div class="feature-content icons numbers">
+          <div class="number">
+            24h
+          </div>
+          <h4 class="black-text">
+            Ullamcorper
+          </h4>
+        </div>
+      </div>
+      <div id="how-to-use" class="feature-wrap">
+        <div data-w-id="0585a81c-ee42-8b56-bc8e-bd582946320a" class="feature-content">
+          <div class="title">
+            Aenean Consectetur Porta
+          </div>
+          <h2 class="black-text">
+            Sem Risus Elit Vestibulum
+          </h2>
+          <p>Nulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. <a href="#">Cras mattis consectetur</a> purus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+        </div>
+        <div class="feature-image">
+          <img
+            src="images/img-003.jpg"
+            width="519"
+            sizes="(max-width: 479px) 90vw, (max-width: 767px) 40vw, (max-width: 991px) 45vw, 27vw"
+            srcset="images/img-003-p-500.jpeg 500w, images/img-003-p-1080.jpeg 1080w, images/img-003-p-1600.jpeg 1600w, images/img-003.jpg 1982w"
+            alt=""
+            class="image-2"
+          >
+        </div>
+      </div>
+      <div class="feature-wrap reverse">
+        <div data-w-id="d79d0682-95d0-be91-6539-b82f492963f4" class="feature-content">
+          <div class="title">
+            Aenean Consectetur Porta
+          </div>
+          <h2 class="black-text">
+            Parturient Tortor
+          </h2>
+          <p>Nulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. <a href="#">Cras mattis consectetur</a> purus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.</p>
+        </div>
+        <div class="feature-image">
+          <img
+            src="images/img-002.jpg"
+            width="519"
+            sizes="(max-width: 479px) 90vw, (max-width: 767px) 40vw, (max-width: 991px) 45vw, 27vw"
+            srcset="images/img-002-p-500.jpeg 500w, images/img-002-p-1080.jpeg 1080w, images/img-002-p-1600.jpeg 1600w, images/img-002.jpg 1982w"
+            alt=""
+            class="image-2"
+          >
+        </div>
+      </div>
+      <div
+        data-animation="slide"
+        data-nav-spacing="8"
+        data-duration="500"
+        data-infinite="1"
+        data-w-id="321c18fb-db93-7e62-3afc-4dab2862f5d4"
+        class="quote-slider w-slider"
+      >
+        <div class="w-slider-mask">
+          <div class="w-slide">
+            <div class="quote-wrap">
+              <img src="images/quotation.svg" width="58" alt="" class="quote-icon">
+              <h3>HeadNulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Pus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.ing</h3>
+              <div class="quote-attribution">
+                <p>Joshua Home</p>
+                <div class="quote-dash" />
+                <p><strong>New York Times</strong></p>
+              </div>
+            </div>
+          </div>
+          <div class="w-slide">
+            <div class="quote-wrap">
+              <img src="images/quotation.svg" width="58" alt="" class="quote-icon">
+              <h3>HeadNulla vitae elit libero, a pharetra augue. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Pus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Etiam porta sem malesuada magna mollis euismod.ing</h3>
+              <div class="quote-attribution">
+                <div class="quote-dash" />
+                <p>Nulla vitae elit libero, a pharetra augue.</p>
+                <p><strong>New York Times</strong></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="left-arrow w-slider-arrow-left">
+          <div class="slider-arrow w-icon-slider-left" />
+        </div>
+        <div class="right-arrow w-slider-arrow-right">
+          <div class="slider-arrow w-icon-slider-right" />
+        </div>
+        <div class="slide-nav w-slider-nav w-slider-nav-invert w-round" />
+      </div>
+      <div data-w-id="23f92271-dba9-fb12-00d3-758ef4d10fb2" class="feature-wrap logos">
+        <img src="images/logos.png" sizes="(max-width: 991px) 90vw, (max-width: 3885px) 70vw, 2720px" srcset="images/logos-p-500.png 500w, images/logos-p-800.png 800w, images/logos-p-1080.png 1080w, images/logos-p-1600.png 1600w, images/logos.png 2720w" alt="">
+      </div>
+    </div>
+    <div class="bottom-image-wrap">
+      <img src="images/bg-apps.jpg" sizes="(max-width: 2500px) 100vw, 2500px" srcset="images/bg-apps-p-500.jpeg 500w, images/bg-apps-p-1080.jpeg 1080w, images/bg-apps-p-2000.jpeg 2000w, images/bg-apps.jpg 2500w" alt="">
+    </div>
+    <div id="download" class="section wide purple">
+      <div data-w-id="376e2a1f-ec70-0dab-11ea-b1863101e37b" class="wrap">
+        <h2>Ready to get started?</h2>
+        <div class="button-wrap">
+          <a href="#" class="button w-button">Download Now</a>
+          <a href="#" class="button contact w-button">Get in Touch</a>
+        </div>
+      </div>
+    </div>
+    <div class="section wide">
+      <div data-w-id="8cf47482-19f1-1f52-25ba-910a60bc91de" class="wrap">
+        <h2>Get updates from Richard</h2>
+        <p>New course alerts, discounts and free lessons</p>
+        <div class="form-block w-form">
+          <form id="email-form" name="email-form" data-name="Email Form" class="form-2">
+            <input
+              id="email-2"
+              type="text"
+              class="input w-input"
+              maxlength="256"
+              name="email-2"
+              data-name="Email 2"
+              placeholder="Enter your email"
+              required=""
+            ><input type="submit" value="Subscribe" data-wait="Please wait..." class="button form-button w-button">
+          </form>
+          <div class="w-form-done">
+            <div>Thank you! Your submission has been received!</div>
+          </div>
+          <div class="w-form-fail">
+            <div>Oops! Something went wrong while submitting the form.</div>
+          </div>
+        </div>
+      </div>
+      <div class="wrapper" />
+    </div>
+    <div class="footer">
+      <div class="wrap">
+        <img src="images/icon-footer.png" width="136" alt="">
+        <div class="footer-list">
+          <a href="#" class="footer-link">Email</a>
+          <a href="#" class="footer-link">Instagram</a>
+          <a href="#" class="footer-link">Facebook</a>
+          <a href="#" class="footer-link">Twitter</a>
+          <a href="/image-license-info" class="footer-link">Image License Info</a>
+          <a href="http://webflow.com" class="footer-link">Powered by Webflow</a>
+        </div>
       </div>
     </div>
   </div>
@@ -30,49 +301,3 @@
 <script>
 export default {}
 </script>
-
-<style>
-/* Sample `apply` at-rules with Tailwind CSS
-.container {
-@apply min-h-screen flex justify-center items-center text-center mx-auto;
-}
-*/
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
