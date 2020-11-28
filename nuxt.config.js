662106076e366adadc1824193f60cca565c18a69
Bartek Igielski <bartek.igielski@snow.dog>
2020-11-28 18:05:05 +0100

    Svh module setup

--- nuxt.config.js
+++ nuxt.config.js
@@ -40,7 +40,8 @@ export default {
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa'
+    '@nuxtjs/pwa',
+    '@nuxtjs/svg'
   ],
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
