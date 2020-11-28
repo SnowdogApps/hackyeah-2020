e2bafbb88fb47318015632fb7c978071265563c8
Bartek Igielski <bartek.igielski@snow.dog>
2020-11-28 18:14:16 +0100

    Page title update

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'hackyeah-2020',
+    title: 'Wateca - Aby woda nie przeciekała nam przez palce',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
