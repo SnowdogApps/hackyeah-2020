4450c2c60318110f4abd954977e57206dc7eed66
rjwaberski <radek.waberski@snow.dog>
2020-11-29 05:15:01 +0100

    Add graph.svg

--- components/global/SummaryBanner.vue
+++ components/global/SummaryBanner.vue
@@ -1,7 +1,9 @@
 <template>
   <section class="relative px-4 sm:px-8 lg:px-16 xl:px-40 2xl:px-64 py-20 text-center bg-light">
     <div class="flex text-lg lg:px-36 xl:flex-row flex-col xl:space-x-10 items-center">
-      <div class="w-full xl:w-1/3 min-w-max xl:mb-0 mb-12 xl:block flex items-center justify-center" />
+      <div class="w-full xl:w-1/3 min-w-max xl:mb-0 mb-12 xl:block flex items-center justify-center" >
+        <graph-image class="mx-auto" />
+      </div>
       <div class="w-full xl:w-2/3 text-left">
         <h2 class="text-4xl font-extrabold text-gray">
           Tylko tyle domostw w Polsce obecnie zbiera wodę deszczową
@@ -12,7 +14,10 @@
 </template>
 
 <script>
+import GraphImage from '~/assets/home/graph.svg?inline'
 export default {
-
+  components: {
+    GraphImage
+  }
 }
 </script>
